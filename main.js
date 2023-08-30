7d9dbfdc17b24fb62152dfc575a37381014fce4f
Lucas Gaspar <gasparlucas8@gmail.com>
2023-08-29 21:33:38 -0300

    Estructura html Finalizada

---
+++ main.js
@@ -0,0 +1,23 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+const btn7 = document.getElementById('btn7');
+const btn8 = document.getElementById('btn8');
+const btn9 = document.getElementById('btn9');
+const btnPunto = document.getElementById('btnPunto');
+// Operaciones Aritmeticas
+const btnDividir = document.getElementById('btnDividir');
+const btnMultiplicar = document.getElementById('btnMultiplicar');
+const btnRestar = document.getElementById('btnRestar');
+const btnSumari = document.getElementById('btnSumari');
+const btnIgual = document.getElementById('btnIgual');
+// Operacion de la calculadora
+const showOperacion = document.getElementById('showOperacion')
+
+function escribirNueve(){
+    showOperacion.tx
+}
+btn9.addEventListener('click',escribirNueve);
